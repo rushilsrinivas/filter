a3b33e4237b49be988dbd743ce842487ed6324f7
rushilsrinivas <76115926+rushilsrinivas@users.noreply.github.com>
2021-07-12 16:53:33 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function preload(){
+}
+function setup(){
+    canvas=createCanvas(640,480);
+    canvas.position(250,250);
+    video=createCapture(VIDEO);
+    video.hide();
+    tint_colour="";
+}
+function draw(){
+    image(video,0,0,640,480);
+    tint(tint_colour);
+}
+function take_snapshot(){
+    save("filter.png");
+}
+ function filter_tint(){
+     tint_colour=document.getElementById("colour").value;
+ }
